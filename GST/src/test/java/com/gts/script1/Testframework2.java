fa9515e698bab2e99d9f938771dfbb84475c7c17
Srilakshmi <Srilakshmi@192.168.1.10>
2020-07-01 21:28:40 +0530

    modified frame work

--- GST/src/test/java/com/gts/script1/Testframework2.java
+++ GST/src/test/java/com/gts/script1/Testframework2.java
@@ -1,5 +1,13 @@
 package com.gts.script1;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Testframework2 {
+	@Test
+	public void tc_02()
+	{
+		Reporter.log("First Test Script",true);
+	}
 
 }
