e4e6b11a44d9008209d0bf06f333b129648f8144
Srilakshmi <Srilakshmi@192.168.1.10>
2020-07-01 23:13:03 +0530

    My Second project imported to github project2

---
+++ GST/src/test/java/com/gts/script1/Testframework3.java
@@ -0,0 +1,16 @@
+package com.gts.script1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testframework3 {
+	
+		@Test
+		public void tc_02()
+		{
+			Reporter.log("Test engineer develops frame other system",true);
+		}
+
+	}
+
+
