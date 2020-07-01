bddd4426ae838c070d5f8f9e316115760499db83
Srilakshmi <Srilakshmi@192.168.1.10>
2020-07-01 20:46:03 +0530

    My First Project into github

---
+++ GST/src/test/java/com/gts/script/Testframework1.java
@@ -0,0 +1,14 @@
+package com.gts.script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testframework1 {
+
+	
+	@Test
+	public void tc_01()
+	{
+		Reporter.log("First Test Script",true);
+	}
+}
